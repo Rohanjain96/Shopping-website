544d2a0cea35d1069a846c618e9720c3960d833e
Rohanjain96 <80338187+Rohanjain96@users.noreply.github.com>
2021-08-19 10:05:21 +0530

    Update index.js
    add to cart update

--- js/index.js
+++ js/index.js
@@ -3,14 +3,32 @@ console.log("javascript added");
 const sidebars = document.getElementById("side-bars");
 const leftbutton = document.getElementById("left-slider-btn");
 const rightbutton = document.getElementById("right-slider-btn");
-const items =document.getElementById("slider-items").children.length;
+const items = document.getElementById("slider-items").children.length;
 const slider = document.getElementById("slider");
+let counter;
+counter = localStorage.getItem("counter");
+console.log("set counter");
+if (counter == null) {
+    counter = 0;
+    localStorage.setItem('counter', counter);
+    document.getElementsByClassName("counter")[0].textContent = counter;
+}
+else {
+    document.getElementsByClassName("counter")[0].textContent = counter;
+}
+
+let productinaddtocart;
+let producttopush;
+console.log(counter);
 let responsiveslider = [
     { left: 1, right: 4 },
     { left: 1, right: 3 },
     { left: 1, right: 2 },
     { left: 1, right: 2 },
 ];
+window.onload = () => {
+    document.getElementsByClassName("counter")[0].textContent = counter;
+};
 function hamburgermenu() {
     sidebars.style.left = '0';
 }
@@ -20,7 +38,7 @@ function clshamburgermenu() {
 
 function rightclicked() {
     leftbutton.style.visibility = 'visible';
-    if (window.outerWidth >= 1201) {
+    if (window.outerWidth >= 1361) {
         if (responsiveslider[0].left <= 3 && responsiveslider[0].right <= items) {
             responsiveslider[0].left += 1;
             responsiveslider[0].right += 1;
@@ -37,7 +55,7 @@ function rightclicked() {
             }
         }
     }
-    else if (window.outerWidth >= 901 && window.outerWidth <= 1200) {
+    else if (window.outerWidth >= 1255 && window.outerWidth <= 1360) {
         if (responsiveslider[1].left <= 4 && responsiveslider[1].right <= items) {
             responsiveslider[1].left += 1;
             responsiveslider[1].right += 1;
@@ -54,7 +72,7 @@ function rightclicked() {
             }
         }
     }
-    else if (window.outerWidth >= 768 && window.outerWidth <= 900) {
+    else if (window.outerWidth >= 768 && window.outerWidth <= 1254) {
         if (responsiveslider[2].left <= 5 && responsiveslider[2].right <= items) {
             responsiveslider[2].left += 1;
             responsiveslider[2].right += 1;
@@ -75,7 +93,7 @@ function rightclicked() {
 
 }
 function leftclicked() {
-    if (window.outerWidth >= 1201) {
+    if (window.outerWidth >= 1361) {
 
         if (responsiveslider[0].left >= 1 && responsiveslider[0].right >= 4) {
             responsiveslider[0].left -= 1;
@@ -96,7 +114,7 @@ function leftclicked() {
             }
         }
     }
-    else if (window.outerWidth >= 901 && window.outerWidth <= 1200) {
+    else if (window.outerWidth >= 1255 && window.outerWidth <= 1360) {
 
         if (responsiveslider[1].left >= 1 && responsiveslider[1].right >= 3) {
             responsiveslider[1].left -= 1;
@@ -117,7 +135,7 @@ function leftclicked() {
             }
         }
     }
-    else if (window.outerWidth >= 768 && window.outerWidth <= 900) {
+    else if (window.outerWidth >= 768 && window.outerWidth <= 1254) {
 
         if (responsiveslider[2].left >= 1 && responsiveslider[2].right >= 2) {
             responsiveslider[2].left -= 1;
@@ -138,5 +156,61 @@ function leftclicked() {
             }
         }
     }
+}
+
+function addtocart(e) {
+    let flag = 0;
+    let detailproduct = {
+        productcode: e.id,
+        productprice: e.children[2].children[1].textContent,
+        producttitle: e.children[2].children[0].textContent,
+        qty: 1,
+        imagepath: e.children[1].children[0].src,
+    };
+    counter = localStorage.getItem("counter");
+    counter++;
+    localStorage.setItem('counter', counter);
+    document.getElementsByClassName("counter")[0].textContent = counter;
+    productinaddtocart = localStorage.getItem("productinaddtocart");
+    if (productinaddtocart == null || productinaddtocart == "[]" ) {
+        producttopush = [];
+        producttopush.push(detailproduct);
+        localStorage.setItem("productinaddtocart", JSON.stringify(producttopush));
+        return;
     }
 
+    else {
+        producttopush = JSON.parse(productinaddtocart);
+    }
+    producttopush.forEach(element => {
+        if (element.productcode == e.id) {
+            console.log("qty increase");
+            element.qty += 1;
+            localStorage.setItem("productinaddtocart", JSON.stringify(producttopush));
+            return;
+
+        }
+        else {
+            console.log(" first push of element");
+            producttopush.push(detailproduct);
+            localStorage.setItem("productinaddtocart", JSON.stringify(producttopush));
+            return;
+        }
+    });
+    function removerepeatedelement(element, index) {
+        productinaddtocart = localStorage.getItem("productinaddtocart");
+        producttopush = JSON.parse(productinaddtocart);
+        for (i = index; i <producttopush.length; i++)
+            {if (producttopush[i].productcode == element.productcode)
+                producttopush.splice(i, i);
+            }
+            localStorage.setItem("productinaddtocart", JSON.stringify(producttopush));   
+    }
+    productinaddtocart = localStorage.getItem("productinaddtocart");
+    producttopush = JSON.parse(productinaddtocart);
+    producttopush.forEach((element, index) => {
+        removerepeatedelement(element, index + 1);
+    });
+
+
+}
