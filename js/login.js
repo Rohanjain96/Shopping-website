da4b65c03dd5240e22e22224c7c3c11efee47a53
Rohanjain96 <rj71289@gmail.com>
2021-09-06 19:08:14 +0530

    removing validation mistake

--- js/login.js
+++ js/login.js
@@ -19,72 +19,81 @@ function showerror(id, error) {
 }
 function clearerror()
 {
+ 
     let formerrors=document.querySelectorAll(".formerror");
-    for(let i=0;i<formerrors.length;i++)
+    console.log(formerrors);
+    for(let item of formerrors)
     {
-        formerrors[i].display='none';
+        console.log("we are in clear error");
+        console.log( item.textContent);
+        item.textContent="";
+        console.log( item.textContent);
     }
 }
 function validatesigninform() {
-    let Email = document.forms['signinform']['signinEmail'].value;
     clearerror();
+    let returnval = 'true';
+    let Email = document.forms['signinform']['signinEmail'].value;
     if (Email.length > 20) {
         showerror("signinEmail","*Email is Too long");
-        return false;
+        returnval=false ;
     }
     else if (Email.search(/.com/)==-1) {
         showerror("signinEmail","* Email is not complete");
-        return false;
+        returnval=false;
     }
     let pswrd = document.forms['signinform']['signinPassword'].value;
      if (pswrd.length<3)
     {
         showerror("signinPassword","* Password should be atleast 3 character long");
-        return false;
+        returnval=false;
     }
+    return returnval;
 }
 function validatesignupform(){
-    let Name = document.forms['signupform']['Name'].value;
     clearerror();
+    let returnval = true;
+    let Name = document.forms['signupform']['Name'].value;
     if (Name.length > 15) {
         showerror("Name","Name cannot be Too long");
-        return false;
+        returnval=false;
     }
-    let Email = document.forms['signinform']['signinEmail'].value;
+    let Email = document.forms['signupform']['signupEmail'].value;
     if (Email.length > 20) {
-        showerror("signinEmail","*Email is Too long");
-        return false;
+        showerror("signupEmail","*Email is Too long");
+        returnval=false;
     }
-    else if (Email.search(/.com/)==-1) {
+     if (Email.search(/.com/)==-1) {
         showerror("signupEmail","* Email is not complete");
-        return false;
+        returnval=false;
     }
     let pswd = document.forms['signupform']['signupPassword'].value;
     let cnfrmpswd = document.forms['signupform']['signupconfirmPassword'].value;
     if (pswd.length<3)
     {
         showerror("signupPassword","* Password should be atleast 3 character long");
-        return false;
+        returnval=false;
     }
     if(pswd.search(/[A-Z]/)==-1)
     {
         showerror("signupPassword","* Password should have one uppercase");
-        return false;  
+        returnval=false;  
     }
     if(pswd.search(/[0-9]/)==-1)
     {
         showerror("signupPassword","* Password should have one number");
-        return false;  
+        returnval=false;  
     }
     if(pswd.search(/[\@\.\-\_]/)==-1)
     {
         showerror("signupPassword","* Password should have one special character");
-        return false;  
+        returnval=false;  
     }
     if(pswd!=cnfrmpswd)
     {
         showerror("signupconfirmPassword","* Password not match");
-        return false;
+        returnval=false;
     }
+    return returnval;
 }
 
